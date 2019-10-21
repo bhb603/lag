0b9a2dc309ae4836d6dadb594cd6811eb85c8944
Brian H. Buchholz <4773480+bhb603@users.noreply.github.com>
2019-10-21 18:32:33 -0400

    add data endpoint

--- README.md
+++ README.md
@@ -16,17 +16,22 @@ OK
 
 ## API
 
-Make an HTTP request with a defined response time:
+Get a delayed response:
 ```sh
 curl http://localhost:8080/?t=10s
 # 10s later
 OK
 ```
 
-Make an HTTP request which responds with 4xx or 5xx errors:
+Get 4xx and 5xx errors:
 ```sh
 curl -i http://localhost:8080/error/418?t=1s
 HTTP/1.1 418 I'm a teapot
 
 I'm a teapot
+```
+
+Download arbitrary amounts of data:
+```sh
+curl http://localhost:8080/data?s=125MB -o data
 ```
--- main.go
+++ main.go
@@ -2,29 +2,70 @@ package main
 
 import (
 	"context"
+	"errors"
 	"flag"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
 	"strconv"
+	"strings"
 	"time"
+	"unicode"
 
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
+)
+
+const (
+	B float64 = 1 << (10 * iota)
+	KB
+	MB
+	GB
 )
 
 var (
 	addr                    string
 	gracefulShutdownTimeout time.Duration
 	maxTimeDelay            time.Duration
+	maxDataSize             uint64
 )
 
 func init() {
 	flag.StringVar(&addr, "addr", ":8080", "server listen address")
 	flag.DurationVar(&gracefulShutdownTimeout, "graceful-timeout", time.Second*30, "duration the server will wait before cancelling idle connections in a graceful shutdown - e.g. 15s or 1m")
 	flag.DurationVar(&maxTimeDelay, "max-time-delay", time.Second*30, "max allowable time delay")
+	flag.Uint64Var(&maxDataSize, "max-data-size", uint64(500*MB), "max allowable data size")
 	flag.Parse()
+}
+
+func parseBytes(s string) (uint64, error) {
+	s = strings.TrimSpace(s)
+	s = strings.ToUpper(s)
+
+	split := strings.IndexFunc(s, unicode.IsLetter)
+	if split == -1 {
+		return 0, errors.New("invalid data size format")
+	}
+
+	byteString, unit := s[:split], s[split:]
+	bytes, err := strconv.ParseFloat(byteString, 64)
+	if err != nil {
+		return 0, errors.New("invalid data size")
+	}
+
+	switch unit {
+	case "B":
+		return uint64(bytes * B), nil
+	case "KB":
+		return uint64(bytes * KB), nil
+	case "MB":
+		return uint64(bytes * MB), nil
+	case "GB":
+		return uint64(bytes * GB), nil
+	default:
+		return 0, errors.New("invalid data unit")
+	}
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
@@ -39,6 +80,20 @@ func errorHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	http.Error(w, http.StatusText(statusCode), statusCode)
+}
+
+func dataHandler(w http.ResponseWriter, r *http.Request) {
+	size, err := parseBytes(r.Form.Get("s"))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	if size > maxDataSize {
+		http.Error(w, "size exceeds maximum", http.StatusBadRequest)
+		return
+	}
+	data := make([]byte, size)
+	w.Write(data)
 }
 
 func healthHandler(w http.ResponseWriter, r *http.Request) {
@@ -81,6 +136,7 @@ func main() {
 
 	// Routes
 	r.HandleFunc("/", indexHandler).Methods("GET")
+	r.HandleFunc("/data", dataHandler).Methods("GET")
 	r.HandleFunc("/error/{status:[0-9]+}", errorHandler).Methods("GET")
 	r.HandleFunc("/health", healthHandler)
 	r.HandleFunc("/", notFoundHandler)
